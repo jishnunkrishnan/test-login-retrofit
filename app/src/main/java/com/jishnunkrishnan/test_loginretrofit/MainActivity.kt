95ba86c302cff5da807a14920781eeceae1e2f53
JIshnu Nk <jishnunkrishnan@gmail.com>
2021-02-24 12:04:50 +0530

    Facebook login implemented

--- app/src/main/java/com/jishnunkrishnan/test_loginretrofit/MainActivity.kt
+++ app/src/main/java/com/jishnunkrishnan/test_loginretrofit/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.jishnunkrishnan.test_loginretrofit
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.GraphRequest
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var callbackManager: CallbackManager
+    private val EMAIL = "email"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        JavaHelper.printHashKey(this)
+        supportActionBar?.hide()
+
+        /* Email Login End Start */
         var defaultEmail = "godwinjoe67@gmail.com"
         var defaultPassword = "123456"
 
@@ -41,5 +58,53 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        /* Email Login End Here */
+
+        btnFacebook.setOnClickListener {
+
+            btnFacebook.setReadPermissions(listOf(EMAIL))
+            callbackManager = CallbackManager.Factory.create()
+
+            LoginManager.getInstance().registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
+
+                override fun onSuccess(result: LoginResult?) {
+
+                    val graphRequest = GraphRequest.newMeRequest(result?.accessToken) { obj, respose ->
+
+                        try {
+
+                            if (obj.has("id")) {
+
+                                Log.i("Fb Data", obj.getString("name"))
+                                Log.i("Fb Data", obj.getString("email"))
+                                Log.i("Fb Data", obj.getString("picture"))
+                            }
+                        } catch (e: Exception) {
+
+                        }
+                    }
+
+                    val param = Bundle()
+                    param.putString("fields", "name, email, id, picture.type(large)")
+                    graphRequest.parameters = param
+
+                    graphRequest.executeAsync()
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+            })
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 }
