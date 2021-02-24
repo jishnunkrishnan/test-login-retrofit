4ee93ae7f2f69feb917fb7e4678dc472dd739fb4
JIshnu Nk <jishnunkrishnan@gmail.com>
2021-02-24 10:59:38 +0530

    Login page design and email login implemented

--- app/src/main/java/com/jishnunkrishnan/test_loginretrofit/MainActivity.kt
+++ app/src/main/java/com/jishnunkrishnan/test_loginretrofit/MainActivity.kt
@@ -2,11 +2,44 @@ package com.jishnunkrishnan.test_loginretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var defaultEmail = "godwinjoe67@gmail.com"
+        var defaultPassword = "123456"
+
+        btnLogin.setOnClickListener {
+
+            var email = etEmail.text.toString()
+            var password = etPassword.text.toString()
+
+            when {
+
+                email.isNullOrEmpty() -> {
+
+                    etEmail.error = "Enter Email"
+                }
+                password.isNullOrEmpty() -> {
+
+                    etPassword.error = "Enter Password"
+                }
+                else -> {
+
+                    if (email == defaultEmail && password == defaultPassword) {
+
+                        Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+                    } else {
+
+                        Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
